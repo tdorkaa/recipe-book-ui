12eb7320c4c383cb993445cd07786764971b7f6f
dtamas <tamasdorkaa@gmail.com>
2020-12-11 11:54:17 +0100

    delete About page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Recipes/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Recipes from "@/components/Recipes.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    Recipes
   }
 };
 </script>
